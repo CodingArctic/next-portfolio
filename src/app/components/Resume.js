de902bc57a826fd8bbdb3458e38e36a5d6899a8d
arctic <geyer.clyde@gmail.com>
2025-05-16 01:05:05 -0400

    slight tweak to resume max width

--- src/app/components/Resume.js
+++ src/app/components/Resume.js
@@ -7,7 +7,7 @@ function Resume() {
             <h1 id="projects" className="text-5xl mb-5">
                 Resume
             </h1>
-            <iframe src="/docs/resume.pdf" className="w-full h-[500px] md:h-[700px] lg:h-[900px] min-w-[300px] sm:max-h-[500px] max-w-7xl self-center flex-auto flex-shrink">
+            <iframe src="/docs/resume.pdf" className="w-full h-[500px] md:h-[700px] lg:h-[900px] min-w-[300px] sm:max-h-[500px] max-w-5xl self-center flex-auto flex-shrink">
             </iframe>
         </div>
     );
