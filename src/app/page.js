ff9ec180391a9559d2a99d60f5ae764635bbf768
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-11 18:42:49 -0500

    remove two different divs for headline text

--- src/app/page.js
+++ src/app/page.js
@@ -9,21 +9,22 @@ export default function Home() {
       <div>
         <div className="container mx-auto px-4 text-center">
           <div>
-            <p className="mt-3 text-5xl">Hi there, my name is <span className="text-blue-400">Clyde</span></p>
+            <p className="mt-3 text-5xl">Hi there, my name is <span className="text-blue-400">Clyde</span>!</p>
           </div>
-          <div className="mt-2">I'm a full-stack developer</div>
-          <div>and fourth-year Web & Mobile Computing Major at RIT</div>
+          <div className="mt-2 m-auto max-w-80 text-wrap">
+            I'm a full-stack developer, and fourth-year Web & Mobile Computing Major at RIT
+          </div>
           <div className="flex justify-around">
             <SocialLink name="linkedin" url="https://www.linkedin.com/in/clydegeyer/"></SocialLink>
             <SocialLink name="github" url="https://www.github.com/CodingArctic/"></SocialLink>
           </div>
         </div>
-        <hr className="m-5 opacity-50" />
+        <hr className="m-5 opacity-50 " />
         <Projects />
         <hr className="m-5 opacity-50" />
         <Resume />
         <footer className="text-center align-middle">
-          <p>made with <FaHeart size={16} className="inline"/> by clyde + next.js</p>
+          <p>made with <FaHeart size={16} className="inline" /> by clyde + next.js</p>
           <p>© 2025 clyde.biz - All Rights Reserved. </p>
         </footer>
       </div>
