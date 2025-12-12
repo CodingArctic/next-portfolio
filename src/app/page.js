1a639d3da3ae1d7d824687b85f60bfa76f2b36b3
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-12 00:02:26 -0500

    fix text margin & width for mobile

--- src/app/page.js
+++ src/app/page.js
@@ -9,12 +9,12 @@ export default function Home() {
       <div>
         <div className="container mx-auto px-4 text-center">
           <div>
-            <p className="mt-3 text-5xl font-light">Hi, my name is <span className="text-blue-400 font-bold">Clyde Geyer</span></p>
+            <p className="mt-3 text-5xl max-w-[80%] m-auto font-light">Hi, my name is <span className="text-blue-400 font-bold">Clyde Geyer</span></p>
           </div>
           <div className="mt-2 m-auto max-w-80 text-wrap font-medium">
             I'm a full-stack developer, and fourth-year Web & Mobile Computing Major at RIT
           </div>
-          <div className="flex justify-around">
+          <div className="flex mt-8 justify-around">
             <SocialLink name="linkedin" url="https://www.linkedin.com/in/clydegeyer/"></SocialLink>
             <SocialLink name="github" url="https://www.github.com/CodingArctic/"></SocialLink>
           </div>
