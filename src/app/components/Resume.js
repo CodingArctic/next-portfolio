65c06bca722c4871d8152b622f3e0b318938d0ba
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-12 00:08:51 -0500

    cache buster

--- src/app/components/Resume.js
+++ src/app/components/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
             <h1 id="projects" className="text-5xl mb-5">
                 Resume
             </h1>
-            <iframe src="/docs/resume.pdf" className="w-full h-[500px] md:h-[700px] lg:h-[900px] min-w-[300px] sm:max-h-[500px] max-w-5xl self-center flex-auto flex-shrink">
+            <iframe src="/docs/2025.12.12.resume.pdf" className="w-full h-[500px] md:h-[700px] lg:h-[900px] min-w-[300px] sm:max-h-[500px] max-w-5xl self-center flex-auto flex-shrink">
             </iframe>
         </div>
     );
