66aad103b5c7ce0b46532aa0e00961b02fe0259c
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-11 21:30:48 -0500

    switch back to static export actually..

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
-
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
-
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
-initOpenNextCloudflareForDev();
--- open-next.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
- 
-export default defineCloudflareConfig({
-  // Using default in-memory cache instead of R2
-});
