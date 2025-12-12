87be7bc3dfad1482c5562a4a012ef2372b5a7aca
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-12 18:20:32 -0500

    adjust size of pdf viewer & mobile resume button

--- src/app/components/Resume.js
+++ src/app/components/Resume.js
@@ -8,16 +8,19 @@ function Resume() {
             <h1 id="projects" className="text-5xl mb-5 font-bold text-[#D5C6E0]">
                 Resume
             </h1>
-            <iframe src={PATH} className="hidden sm:block w-full h-[500px] md:h-[700px] lg:h-[900px] min-w-[300px] sm:max-h-[500px] max-w-5xl self-center flex-auto flex-shrink">
+            <iframe src={PATH} className="hidden sm:block w-5/6 h-[500px] md:h-[700px] lg:h-[900px] min-w-[300px] sm:max-h-[500px] max-w-5xl self-center flex-auto flex-shrink">
             </iframe>
-            <Link href={PATH}
-                target="blank"
-                className="block sm:hidden p-4 bg-[#BA5A31] rounded-md"
-            >
-                <p className="font-light m-auto text-center">
-                    Not enough width! Click here for full screen.
-                </p>
-            </Link>
+                <Link href={PATH}
+                    target="blank"
+                    className="block sm:hidden p-2 bg-[#BA5A31] m-auto rounded-md w-9/12"
+                >
+                    <p className="font-light text-center text-wrap">
+                        Not enough width!
+                    </p>
+                    <p className="font-light text-center">
+                        Click here for full screen.
+                    </p>
+                </Link>
         </div>
     );
 }
