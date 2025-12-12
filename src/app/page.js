d2ff31de931fa90672ee700030fbfcf31762edb3
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-11 22:45:11 -0500

    change language and font weight

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,9 @@ export default function Home() {
       <div>
         <div className="container mx-auto px-4 text-center">
           <div>
-            <p className="mt-3 text-5xl">Hi there, my name is <span className="text-blue-400">Clyde</span>!</p>
+            <p className="mt-3 text-5xl font-light">Hi, my name is <span className="text-blue-400 font-bold">Clyde Geyer</span></p>
           </div>
-          <div className="mt-2 m-auto max-w-80 text-wrap">
+          <div className="mt-2 m-auto max-w-80 text-wrap font-medium">
             I'm a full-stack developer, and fourth-year Web & Mobile Computing Major at RIT
           </div>
           <div className="flex justify-around">
