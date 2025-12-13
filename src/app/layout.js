da6782c0d301e16bba1addae0f8fb0713177217a
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-12 19:08:32 -0500

    remove unused font from layout & set bg color to prevent flash

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { NavbarSimple } from "./components/Navbar.js"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Clyde Geyer",
@@ -20,10 +9,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" style={{backgroundColor: "#0a0a0a", color: "#ededed"}}>
+      <body className="antialiased">
         <NavbarSimple/>
         {children}
       </body>
