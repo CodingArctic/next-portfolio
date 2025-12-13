44d817f388293ce22eba50b53e42179a8f179f5e
Clyde Geyer <geyer.clyde@gmail.com>
2025-12-12 19:09:09 -0500

    lazy load project images

--- src/app/components/ProjectCard.js
+++ src/app/components/ProjectCard.js
@@ -43,6 +43,7 @@ const ProjectCard = ({ title, link, github, image, description, techStack = [] }
                     width={600}
                     height={400}
                     alt={title}
+                    loading="lazy"
                     className="rounded-md m-auto border-3 border-background w-full h-auto md:h-100 md:object-cover md:object-top transition-transform duration-300"
                 />
                 <div className="flex mt-2 place-content-between items-center">
